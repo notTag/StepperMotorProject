3d06a4b968d9fe8093e82f2c3be09dd26762c17d
Nick Tag <nick.tagliasacchi@gmail.com>
2016-12-08 05:08:15 -0500

    Troubleshooting blynk connection & removed potentiometer
    - There is no widget selection screen within the app itself. Suspect of
    cause is out of date iPhone fw.
    -Removed pot. from hardware setup. Removed variable in code.

--- StepperMotorProject.ino
+++ StepperMotorProject.ino
@@ -1,55 +1,16 @@
 #include <BlynkApiArduino.h>
-#include <BlynkApiParticle.h>
-#include <BlynkParticle.h>
-#include <BlynkSimpleCC3000.h>
-#include <BlynkSimpleCurieBLE.h>
-#include <BlynkSimpleEnergiaEthernet.h>
-#include <BlynkSimpleEnergiaWiFi.h>
-#include <BlynkSimpleEsp32.h>
-#include <BlynkSimpleEsp8266.h>
-#include <BlynkSimpleEsp8266_SSL.h>
-#include <BlynkSimpleEthernet.h>
-#include <BlynkSimpleEthernet2.h>
-#include <BlynkSimpleEthernetV2_0.h>
-#include <BlynkSimpleFishino.h>
-#include <BlynkSimpleIntelEdisonWiFi.h>
-#include <BlynkSimpleLinkItONE.h>
-#include <BlynkSimpleMKR1000.h>
-#include <BlynkSimpleParticle.h>
-#include <BlynkSimpleRedBear_Duo.h>
-#include <BlynkSimpleRedBear_Duo_BLE.h>
-#include <BlynkSimpleRedBearLab_BLE_Nano.h>
-#include <BlynkSimpleRFduinoBLE.h>
-#include <BlynkSimpleSerialBLE.h>
-#include <BlynkSimpleShieldEsp8266.h>
-#include <BlynkSimpleSimbleeBLE.h>
+
+#include <SoftwareSerial.h>
+SoftwareSerial DebugSerial(8,9); //Rx, Tx
+
+#define BLYNK_PRINT DebugSerial
 #include <BlynkSimpleStream.h>
-#include <BlynkSimpleTinyDuino.h>
-#include <BlynkSimpleUIPEthernet.h>
-#include <BlynkSimpleUserDefined.h>
-#include <BlynkSimpleWifi.h>
-#include <BlynkSimpleWiFiShield101.h>
-#include <BlynkSimpleWiFiShield101_SSL.h>
-#include <BlynkSimpleWiFly.h>
-#include <BlynkSimpleWildFire.h>
-#include <BlynkSimpleYun.h>
-#include <BlynkWidgets.h>
-#include <BlynkWrongInclude.h>
-#include <WidgetBridge.h>
-#include <WidgetGPS.h>
-#include <WidgetLCD.h>
-#include <WidgetLED.h>
-#include <WidgetRTC.h>
-#include <WidgetSD.h>
-#include <WidgetTable.h>
-#include <WidgetTerminal.h>
-#include <WidgetTimeInput.h>
 
-
+char auth[] = "bfb4b59a268a471f9b0ec05b31db8b52";
 
 int dirpin = 2;   //direction pin on pin 2
 int steppin = 4; //step pin on pin 4
-int potPin = 0; //5 volt potentiometer on pin 0
+int delayTime = 100;
 
 void setup() {
   DebugSerial.begin(9600);
@@ -64,11 +25,9 @@ void setup() {
 
 void loop() {
   Blynk.run();
-  int potDelay = analogRead(potPin)/3;
-  Serial.println(potDelay);
 
   digitalWrite(steppin, HIGH);
-  delayMicroseconds(potDelay);
+  delayMicroseconds(delayTime);
   digitalWrite(steppin, LOW);
-  delayMicroseconds(potDelay);
+  delayMicroseconds(delayTime);
 }
